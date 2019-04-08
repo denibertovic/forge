efdd16c2b7038cede0326cb22b9598bac2e7443a
Deni Bertovic <deni@denibertovic.com>
2019-04-09 00:10:20 +0200

    Fixes warnings in fetch command

--- src/Forge/Lib.hs
+++ src/Forge/Lib.hs
@@ -15,6 +15,9 @@ import qualified Data.Text as T
 import System.Exit (die)
 import RIO.Directory (doesFileExist, renameFile)
 
+errMsg :: String
+errMsg = "Please specify a template name in the format 'myorg/templatename'. See here for a list of my default templates: https://github.com/denibertovic/makefiles."
+
 entrypoint :: ForgeOpts -> IO ()
 entrypoint (ForgeOpts _ cmd) = do
   case cmd of
@@ -29,12 +32,15 @@ fetchTemplate m = do
         "https://raw.githubusercontent.com/denibertovic/makefiles/master/"
   case m of
     Nothing ->
-      die
-        "Please specify a template name in the format 'myorg/templatename'. See here for a list of my default templates: https://github.com/denibertovic/makefiles."
+      die errMsg
     Just (MakefileTemplateName name) -> do
-      let (b', n') = case T.splitOn "/" name of
-                [_] -> (baseUrl, name)
-                [group, name'] -> (T.replace "denibertovic" group baseUrl, name')
-      exists <- doesFileExist "Makefile"
-      when exists (renameFile "Makefile" "Makefile.old")
-      downloadFile (b' <> n' <> ".makefile") "Makefile"
+      let ret = case T.splitOn "/" name of
+                [_] -> Just (baseUrl, name)
+                [group, name'] -> Just (T.replace "denibertovic" group baseUrl, name')
+                _ -> Nothing
+      case ret of
+        Nothing -> die $ "Failed to parse. " <> errMsg
+        Just (b', n') -> do
+            exists <- doesFileExist "Makefile"
+            when exists (renameFile "Makefile" "Makefile.old")
+            downloadFile (b' <> n' <> ".makefile") "Makefile"
